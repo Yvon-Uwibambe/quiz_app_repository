58ec11e09b15314ec33c190427ded3636fef3d41
Yvon-Uwibambe <yvonbrowny@gmail.com>
2022-08-23 18:24:20 +0200

    changed the title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quiz app for Questions',
       theme: ThemeData(
         // This is the theme of your application.
         //
